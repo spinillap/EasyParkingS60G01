4ac17be47e44fe7a28d8c6cb10533a25d1de278d
Andrés Murillo <68217275+afmurillor@users.noreply.github.com>
2021-09-23 00:28:16 -0500

    Add files via upload
    Añadir CRUD para el módulo de usuarios

---
+++ EasyParking/src/java/restful/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package restful;
+
+import java.util.Set;
+import javax.ws.rs.core.Application;
+
+/**
+ *
+ * @author afmur
+ */
+@javax.ws.rs.ApplicationPath("api")
+public class ApplicationConfig extends Application {
+    
+}
